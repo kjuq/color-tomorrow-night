f597bc4ca2e0bba12950245d3c4b1f5bec5cacac
kjuq <44694396+kjuq@users.noreply.github.com>
2022-07-17 06:32:15 +0900

    initial commit

---
+++ conf.d/color_tomorrow_night.fish
@@ -0,0 +1,45 @@
+# black     161821
+# red       e27878
+# green     b4be82
+# yellow    e2a478
+# blue      84a0c6
+# magenta   a093c7
+# cyan      89b8c2
+# white     c6c8d1
+# brblack   6b7089
+# brred     e98989
+# brgreen   c0ca8e
+# bryellow  e9b189
+# brblue    91acd1
+# brmagenta ada0d3
+# brcyan    95c4ce
+# brwhite   d2d4de
+
+# Colorscheme: Tomorrow Night
+set -U fish_color_normal normal
+set -U fish_color_command c6c8d1 #b294bb
+set -U fish_color_quote b5bd68
+set -U fish_color_redirection 8abeb7
+set -U fish_color_end b294bb
+set -U fish_color_error cc6666
+set -U fish_color_param 81a2be
+set -U fish_color_comment f0c674
+set -U fish_color_match --background=91acd1
+set -U fish_color_selection c6c8d1 --bold --background=6b7089
+set -U fish_color_search_match e9b189 --background=6b7089
+set -U fish_color_history_current --bold
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd b4be82
+set -U fish_color_cwd_root e27878
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion 969896
+set -U fish_color_user c0ca8e
+set -U fish_color_host normal
+set -U fish_color_cancel --reverse
+set -U fish_pager_color_prefix normal --bold --underline
+set -U fish_pager_color_progress d2d4de --background=89b8c2
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D
+set -U fish_pager_color_selected_background --background=6b7089
+
